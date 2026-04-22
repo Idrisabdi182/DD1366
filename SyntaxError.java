849f2120da9fd09e692e4d193a497091f5f0740b
Idrisabdi182 <152632180+Idrisabdi182@users.noreply.github.com>
2026-04-22 15:30:05 +0200

    Update SyntaxError.java

--- SyntaxError.java
+++ SyntaxError.java
@@ -1,6 +1,18 @@
-// Klass för att representera syntaxfel.  I praktiken vill man nog
-// även ha med ett litet felmeddelande om *vad* som var fel, samt på
-// vilken rad/position felet uppstod
-// Författare: Idris Mahmud Abdi
+
+/** Klass för att representera syntaxfel.  I praktiken vill man nog
+ * även ha med ett litet felmeddelande om *vad* som var fel, samt på
+ * vilken rad/position felet uppstod
+ */
 public class SyntaxError extends Exception {
+    private int row;
+
+    // Constructor
+    public SyntaxError(int row) {
+        super("Syntaxfel på rad " + row);
+        this.row = row;
+    }
+
+    public int getRow() {
+        return row;
+    }
 }
