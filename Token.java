382632e7f07369d2ee7e7c808011b1462c200d3a
Idrisabdi182 <152632180+Idrisabdi182@users.noreply.github.com>
2026-04-22 15:30:18 +0200

    Update Token.java

--- Token.java
+++ Token.java
@@ -1,36 +1,41 @@
-// Författare: Idris Mahmud Abdi
-
-// De olika token-typer vi har i grammatiken
 enum TokenType {
-    FORW, BACK, LEFT, RIGHT, DOWN, UP, COLOR, REP, PERIOD, QUOTE, DECIMAL, HEX, ERROR
+    FORW, BACK, LEFT, RIGHT, DOWN, UP, COLOR, REP, PERIOD, QUOTE, DECIMAL, HEX, ERROR, EOF
 }
 
-/** Klass för att representera en token
- * I praktiken vill man nog även spara info om vilken rad/position i
- * indata som varje token kommer ifrån, för att kunna ge bättre
- * felmeddelanden
- */
 class Token {
     private TokenType type;
     private Object data;
+    private int row;
 
-    public Token(TokenType type) {
+    public Token(TokenType type, int row) {
         this.type = type;
         this.data = null;
+        this.row = row;
     }
 
-    public Token(TokenType type, Object data) {
+    public Token(TokenType type, Object data, int row) {
         this.type = type;
         this.data = data;
+        this.row = row;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public int getRow() {
+        return row;
     }
 
     public String toString() {
-        if (data == null)
+        if (data == null) {
             return type.toString();
-        else
-            return type.toString() + "(" + data.toString() + ")";
+        } else {
+            return type + "(" + data + ")";
+        }
     }
-
-    public TokenType getType() { return type; }
-    public Object getData() { return data; }
 }
